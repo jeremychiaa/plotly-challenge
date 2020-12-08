c450be4f9ff46423d11fb81ffeb888c5d11d2f77
Jeremy Chia <jeremychiajianzhi@gmail.com>
2020-12-08 18:10:22 +1100

    otu barh

--- static/js/app.js
+++ static/js/app.js
@@ -23,6 +23,34 @@ d3.json("samples.json").then((sampleData) => {
 
         // Sort new object by sample values
         var sortedPatientData = patientData.sort((a, b) => b.sample_values - a.sample_values);
-    }
+
+        // Find first 10 values for first patient
+        var sampleValues = sortedData[i].sample_values.slice(0, 10).reverse();
+        var otuIds = sortedData[i].otu_ids.slice(0, 10);
+        var otuLabels = sortedData[i].otu_labels.slice(0, 10);
+
+        // Create empty array for chart labels
+        var chartLabels = [];
+
+        // Concatenate OTU ID to display on chart
+        otuIds.forEach(x => chartLabels.push("OTU " + x));
+
+        // Setting the data to plot
+        var data = [{
+            type: "bar",
+            x: sampleValues,
+            y: chartLabels.reverse(),
+            orientation: "h",
+            text: otuLabels
+        }]
+
+        // Setting the plot title
+        var layout = {
+            title: "Top 10 OTUs"
+        }
+
+        // Using Plotly to visualise horizontal bar chart
+        Plotly.newPlot("bar", data, layout, {displayModeBar: false});
+    };
 
 });
