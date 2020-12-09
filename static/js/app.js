8246972a6fd6b78185d13499cdc16de1a7f87149
Jeremy Chia <jeremychiajianzhi@gmail.com>
2020-12-09 12:23:36 +1100

    gauge chart and cleanup

--- static/js/app.js
+++ static/js/app.js
@@ -101,6 +101,7 @@ d3.json("samples.json").then((sampleData) => {
         
         console.log(panelData);
 
+        // Gauge chart
         var data = [
             {
                 domain: { x: [0, 1], y: [0, 1] },
@@ -111,9 +112,14 @@ d3.json("samples.json").then((sampleData) => {
             }
         ];
         
-        var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
-        Plotly.newPlot('gauge', data, layout);
+        var layout = 
+            {
+                width: 600,
+                height: 500,
+                margin: { t: 0, b: 0 }
+            };
 
+        Plotly.newPlot('gauge', data, layout);
     };
 
     // Set up event listener
